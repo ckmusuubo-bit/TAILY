01cce0f52645eca94628120cb68bec05c9060518
ckmusuubo-bit <c.k.musuubo@gmail.com>
2025-10-20 06:26:33 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2c3e50',
+        secondary: '#3498db'
+      }
+    },
+  },
+  plugins: [],
+}
